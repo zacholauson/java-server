74015cd0fe6f9062f4c1905c9467b4cd06b2d3b0
Zach Olauson <zmolauson@gmail.com>
2014-04-02 14:30:42 -0500

    organize routes better

--- src/main/Server.java
+++ src/main/Server.java
@@ -28,7 +28,8 @@ public class Server {
         port         = _port;
         directory    = _directory;
 
-        initializeRoutes();
+//        initializeRoutes();
+        initializeCobSpecRoutes();
 
         ServerSocket serverSocket = newServerSocket(port);
         ExecutorService executor = newThreadPool();
@@ -45,27 +46,26 @@ public class Server {
         }
     }
 
-//    private static void initializeRoutes() {
-//        Router.addRoute("GET", "/",                   new TextRoute("Hello World"));
-//        Router.addRoute("GET", "/image",              new FileRoute("/public/pic.png"));
-//        Router.addRoute("GET", "/directory",          new GetDirectoryRoute(getDirectory(), "/public"));
-//        Router.addRoute("OPTIONS", "/method_options", new OptionsRoute("GET,HEAD,POST,OPTIONS,PUT"));
-//    }
-
-    // cob_spec routes
     private static void initializeRoutes() {
-        Router.addRoute("GET", "/",                   new GetDirectoryRoute(getDirectory(), "/"));
-        Router.addRoute("POST", "/form",              new PostFormData());
-        Router.addRoute("PUT", "/form",               new PutFormData());
+        Router.addRoute("GET", "/",                   new TextRoute("Hello World"));
+        Router.addRoute("GET", "/image",              new FileRoute("/public/pic.png"));
+        Router.addRoute("GET", "/directory",          new GetDirectoryRoute(getDirectory(), "/public"));
         Router.addRoute("OPTIONS", "/method_options", new OptionsRoute("GET,HEAD,POST,OPTIONS,PUT"));
-        Router.addRoute("GET", "/file1",              new FileRoute("/file1"));
-        Router.addRoute("GET", "/image.jpeg",         new FileRoute("/image.jpeg"));
-        Router.addRoute("GET", "/image.png",          new FileRoute("/image.png"));
-        Router.addRoute("GET", "/image.gif",          new FileRoute("/image.gif"));
-        Router.addRoute("PUT", "/file1",              new MethodNotAllowedRoute());
-        Router.addRoute("POST", "/text-file.txt",     new MethodNotAllowedRoute());
-                                                                       // find way to make this dynamic
-        Router.addRoute("GET", "/redirect",           new RedirectRoute("http://localhost:5000/"));
+    }
+
+    private static void initializeCobSpecRoutes() {
+        Router.addRoute("GET",     "/",               new GetDirectoryRoute(getDirectory(), "/"));
+        Router.addRoute("POST",    "/form",           new PostFormData());
+        Router.addRoute("PUT",     "/form",           new PutFormData());
+        Router.addRoute("OPTIONS", "/method_options", new OptionsRoute("GET,HEAD,POST,OPTIONS,PUT"));
+        Router.addRoute("GET",     "/file1",          new FileRoute("/file1"));
+        Router.addRoute("GET",     "/image.jpeg",     new FileRoute("/image.jpeg"));
+        Router.addRoute("GET",     "/image.png",      new FileRoute("/image.png"));
+        Router.addRoute("GET",     "/image.gif",      new FileRoute("/image.gif"));
+        Router.addRoute("PUT",     "/file1",          new MethodNotAllowedRoute());
+        Router.addRoute("POST",    "/text-file.txt",  new MethodNotAllowedRoute());
+        Router.addRoute("GET",     "/redirect",       new RedirectRoute("http://localhost:5000/"));
+        Router.addRoute("GET",     "/parameters",     new GetParamsRoute());
     }
 
     private static ExecutorService newThreadPool() {
