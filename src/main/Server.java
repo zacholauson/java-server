f9977a40c8227241178f5c26bb27af20bf067be3
Zach Olauson <zmolauson@gmail.com>
2014-04-09 10:00:32 -0500

    refactor server

--- src/main/Server.java
+++ src/main/Server.java
@@ -11,6 +11,7 @@ import main.routing.RouteInitializers;
 import main.socket.ISocket;
 import main.socket.socketWrappers.SocketWrapper;
 
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -18,49 +19,19 @@ import java.util.concurrent.Executors;
 public class Server {
     public  static final ILogger LOGGER = newLogger();
     private static       String  directory;
+    public  static       ServerSocket serverSocket;
     public  static       String  getDirectory() { return directory; }
 
-    public static void start(int port, String _directory) throws Exception {
+    public static void start(int port, String _directory) {
         directory = _directory;
+        serverSocket = newServerSocket(port);
 
         RouteInitializers.cobSpecRoutes();
-        ServerSocket serverSocket = newServerSocket(port);
-        ExecutorService executor = newThreadPool();
+        ExecutorService executor = Executors.newFixedThreadPool(16);
         while(!serverSocket.isClosed()) {
-            ISocket socketWrapper = null;
-
-            try {
-                socketWrapper = new SocketWrapper(serverSocket.accept());
-                executor.execute(newThread(socketWrapper, newRequest(socketWrapper), newResponse()));
-            } catch (Exception exception) {
-                if (socketWrapper != null) {
-                    socketWrapper.getSocket().close();
-                } else {
-                    serverSocket.close();
-                    exception.printStackTrace();
-                }
-            }
+            ISocket socketWrapper = wrapClientSocket(serverSocket);
+            executor.execute(newThread(socketWrapper, newRequest(socketWrapper), newResponse()));
         }
-    }
-
-    private static ExecutorService newThreadPool() {
-        return Executors.newFixedThreadPool(16);
-    }
-
-    private static ConnectionWrapper wrapConnection(ISocket socketWrapper, IRequest request, IResponse response) throws Exception {
-        return new ConnectionWrapper(socketWrapper, request, response);
-    }
-
-    private static Runnable newThread(ISocket socketWrapper, IRequest request, IResponse response) throws Exception {
-        return new Thread(wrapConnection(socketWrapper, request, response));
-    }
-
-    private static ServerSocket newServerSocket(int port) throws Exception {
-        return new ServerSocket(port);
-    }
-
-    private static IRequest newRequest(ISocket socketWrapper) throws Exception {
-        return new HTTPRequest(socketWrapper.socketInputStream());
     }
 
     private static IResponse newResponse() {
@@ -69,5 +40,46 @@ public class Server {
 
     private static ILogger newLogger() {
         return new Logger();
+    }
+
+    private static ConnectionWrapper wrapConnection(ISocket socketWrapper, IRequest request, IResponse response) {
+        return new ConnectionWrapper(socketWrapper, request, response);
+    }
+
+    private static Runnable newThread(ISocket socketWrapper, IRequest request, IResponse response) {
+        return new Thread(wrapConnection(socketWrapper, request, response));
+    }
+
+    private static ServerSocket newServerSocket(int port) {
+        ServerSocket serverSocket = null;
+        try {
+            serverSocket = new ServerSocket(port);
+        } catch (IOException e) {
+            new Exception("Failed to start server socket").printStackTrace();
+            e.printStackTrace();
+        }
+        return serverSocket;
+    }
+
+    private static ISocket wrapClientSocket(ServerSocket serverSocket) {
+        ISocket socketWrapper = null;
+        try {
+            socketWrapper = new SocketWrapper(serverSocket.accept());
+        } catch (IOException e) {
+            new Exception("Failed to create client connection").printStackTrace();
+            e.printStackTrace();
+        }
+        return socketWrapper;
+    }
+
+    private static IRequest newRequest(ISocket socketWrapper) {
+        IRequest request = null;
+        try {
+            request = new HTTPRequest(socketWrapper.socketInputStream());
+        } catch (IOException e) {
+            new Exception("Failed to parse request").printStackTrace();
+            e.printStackTrace();
+        }
+        return request;
     }
 }
---
+++ src/test/ServerTest.java
@@ -0,0 +1,41 @@
+package test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import static org.junit.Assert.*;
+
+@RunWith(JUnit4.class)
+public class ServerTest {
+    @Test
+    public void clientConnectionTest() {
+        Socket socket = new Socket();
+        InetSocketAddress endPoint;
+        try {
+            endPoint = new InetSocketAddress(InetAddress.getLocalHost().getHostAddress(), 9000);
+            if (!endPoint.isUnresolved()) {
+                assertFalse(endPoint.isUnresolved());
+                try {
+                    socket.connect(endPoint, 2000);
+                    assertTrue(socket.isConnected());
+                } catch (IOException e) {
+                    System.err.println("NOTE: Server must be running on port 9000, for ServerTest.clientConnectionTest to run\n");
+                    e.printStackTrace();
+                    fail();
+                }
+            } else {
+                new Exception("End Point Unresolved").printStackTrace();
+            }
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+            fail();
+        }
+    }
+}
